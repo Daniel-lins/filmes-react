93fb7eafad0d45e033fc8889177387546308c369
Daniel Lins <dlins300@gmail.com>
2021-09-01 23:27:02 -0300

    Requisição e teste da api e começo da criação da pagina recentes

--- src/Pages/Recentes/Recentes.js
+++ src/Pages/Recentes/Recentes.js
@@ -1,7 +1,30 @@
+import axios from "axios";
+import {useState, useEffect} from 'react';
+
+
+
 function Recentes() {
+    const [content, setContent] = useState([]);
+    const fetchTrending = async ()=> {
+        const { data } = await axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`
+        
+        );
+
+       
+        setContent(data.results);
+
+    }
+
+    useEffect(() => {
+        fetchTrending();
+    }, [])
     return (
-        <div>
-            <span className='pageTitle'>Recentes</span>
+        <div><span className='pageTitle'>Recentes</span>
+
+        <div className="trending">
+
+        { content && content.map((c) => (console.log(c)))}
+</div>
         </div>
     )
 }
