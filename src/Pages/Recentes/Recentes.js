08165a10ecebd9b2e2965c525d6af9c055cd6535
Daniel Lins <dlins300@gmail.com>
2021-09-02 22:36:48 -0300

    .

--- src/Pages/Recentes/Recentes.js
+++ src/Pages/Recentes/Recentes.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import {useState, useEffect} from 'react';
-
+import '../../components/SingleContent/SingleContent'
+import SingleContent from "../../components/SingleContent/SingleContent";
 
 
 function Recentes() {
@@ -23,7 +24,20 @@ function Recentes() {
 
         <div className="trending">
 
-        { content && content.map((c) => (console.log(c)))}
+        { content && content.map((c) => (   
+            <SingleContent 
+            key={c.id}
+            id={c.id}
+            poster={c.poster_path}
+            title={c.title || c.name}
+            date={c.first_air_date || c.release_date}
+            media_type={c.media_type}
+            vote_average={c.vote_average}
+            
+            
+            />
+
+        ))}
 </div>
         </div>
     )
